66dd8a0557baba9cf8bd7fa4a4146ba9038e6ee7
Mzigi <smrengen@gmail.com>
2025-03-10 21:42:14 +0100

    Removed console.log(RSSI)

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -69,7 +69,6 @@ export default function Dashboard({ serialData, stationPositions, sendSerial, se
         let txPower = 17;
         let [R1, R2, R3] = [rssiToDistance(serialDataGroup.S.S1, txPower, rssi[0]), rssiToDistance(serialDataGroup.S.S2, txPower, rssi[1]), rssiToDistance(serialDataGroup.S.S3, txPower, rssi[2])];
         let resultPos = basicTriangulation(new Vector3(0), new Vector3(-stationPositions[1].x,0), new Vector3(-stationPositions[2].x,stationPositions[2].z), R1, R2, R3);
-        console.log(rssi);
         return resultPos;
     }
     test3dGD.indexFunc = millisIndexFunc;
